44f2550f6c5a74b8c8279cd47c218441fd1cb94e
giorgiopea <annatar93@gmail.com>
2015-11-17 22:02:16 +0100

    auth

--- app/app.module.js
+++ app/app.module.js
@@ -1,6 +1,4 @@
 (function(){
-  angular.module('Plunner',['ngRoute','ngCookies'])
-  .run(['$http', '$cookies', function($http, $cookies) {
-  $http.defaults.headers.post['X-XSRF-TOKEN'] = $cookies.get('X-XSRF-TOKEN');
+  angular.module('Plunner',['ngRoute','ngCookies']);
 }]);
 }());
--- app/shared/signinServices/authService.js
+++ app/shared/signinServices/authService.js
@@ -10,7 +10,8 @@
         data : 'email='+credentials.email + '&password='+credentials.pwd,
         withCredentials : true,
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+          'X-XSRF-TOKEN' : $cookies.get('XSRF-TOKEN')
         }
       })
       .then(function(response){
