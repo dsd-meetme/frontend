de0ae08c4b6d9c6ef56f30c6d4fa8ac799030e80
giorgiopea <annatar93@gmail.com>
2015-11-17 21:52:31 +0100

    continue auth

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,6 @@
 (function(){
   angular.module('Plunner',['ngRoute','ngCookies']);
+  .run(['$http', '$cookies', function($http, $cookies) {
+  $http.defaults.headers.post['X-XSRF-TOKEN'] = $cookies.get('X-XSRF-TOKEN');
+}]);
 }());
