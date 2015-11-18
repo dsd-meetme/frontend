1b35a758ae74ad9521b148b90cecf2ab9e973754
giorgiopea <annatar93@gmail.com>
2015-11-18 19:28:55 +0100

    jwt impl

--- app/shared/signinServices/authService.js
+++ app/shared/signinServices/authService.js
@@ -6,6 +6,7 @@
             method : 'POST',
             url : '//api.plunner.com/auth/login',
             data : 'email='+credentials.email + '&password='+credentials.pwd,
+            headers: {'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
             withCredentials : true,
           }).then(
             function(response){
