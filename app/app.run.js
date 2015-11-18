f8d4ac70f8853b65d9eb6986eee9983b452442db
giorgiopea <annatar93@gmail.com>
2015-11-18 21:51:04 +0100

    finished basic auth

--- app/app.run.js
+++ app/app.run.js
@@ -8,9 +8,6 @@
           else if(next.controller==='usiController' && $cookies.get('auth_token') !== undefined){
             $location.path('/dashboard');
           }
-          else if (next.controller==='logoutController' && $cookies.get('auth_token') !== undefined) {
-            $location.path('/userSignIn');
-          }
   });
 });
 }())
