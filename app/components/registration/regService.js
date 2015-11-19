6254352e11f0528f52c2b5675cd1081d449add02
giorgiopea <annatar93@gmail.com>
2015-11-19 16:42:14 +0100

    continue registration

--- app/components/registration/regService.js
+++ app/components/registration/regService.js
@@ -4,16 +4,16 @@
       register : function(data){
         $http({
           method : 'POST',
-          url : '//api.plunner.com/register',
+          url : '//api.plunner.com/auth/register',
           data : 'name='+data.name + '&email='+data.email+'&password='+data.pwd,
           headers: {'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'},
           withCredentials : true
         }).then(
           function(response){
-
+            console.log(response);
           },
           function(response){
-
+            console.log(response);
           }
         )
       }
