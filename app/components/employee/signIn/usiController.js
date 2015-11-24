aff4980c03325cf06b4034a6f7071b7ea0d9e4c0
jean-barre <barre.jean.63@gmail.com>
2015-11-24 01:31:19 +0100

    req1 progression
    - re-use of the organization model « dashboard+signin » for the
    employee (naming conflict for the dashboard)
    
    - integration of some basic bootstrap : navigation bar and registration
    form
    
    - integration of open sans
    
    +personal web technologies practice

---
+++ app/components/employee/signIn/usiController.js
@@ -0,0 +1,38 @@
+(function(){
+  /**
+  A controller for managing the login of Users and Administrators in the context
+  of a plunner organization
+  @author Giorgio Pea
+  @param loginService A service that is used to manage the login of a plunner's organization
+  **/
+  var controller = function($rootScope,$scope,$location,loginService){
+    var self = this;
+
+    self.errors = {};
+    //an object that encapsulate the validity status of input fields
+    self.validFields = {
+      inputReq : false,
+      emailReq : false,
+      emailVal : false
+    }
+
+    self.login = function(){
+      //Processes the submit of usiForm (organization sign in)
+      var form = $scope.usiForm;
+      //Validity status of input fields checking
+      self.validFields.inputReq = form.usiPwd.$error.required;
+      self.validFields.emailReq = form.usiEmail.$error.required;
+      self.validFields.emailVal = form.usiEmail.$error.email;
+      if(!form.$invalid){
+        loginService.login('http://api.plunner.com/employee/auth/login',{
+          email : self.usiEmail,
+          pwd : self.usiPwd,
+          rmbMe : self.rmbMe
+        },self.errors,'/employee');
+      }
+    }
+  }
+  
+  var app = angular.module('Plunner');
+  app.controller('usiController',controller);
+}())
