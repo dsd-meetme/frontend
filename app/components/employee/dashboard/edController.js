73697bc5f9c58e0ffca014a62ea0954f34a573f9
giorgiopea <annatar93@gmail.com>
2015-11-25 22:14:04 +0100

    update package.json

--- app/components/employee/dashboard/edController.js
+++ app/components/employee/dashboard/edController.js
@@ -5,12 +5,30 @@
   @param logoutService A service used to manage the logout of a plunner's organization
   **/
   var controller = function(logoutService,dataProvider){
-    this.errors = {
+    var c = this;
+    c.errors = {
       unauthorized : false,
       forbidden : false
     }
-    this.data = {};
-    this.logout = function(){
+    c.meetingsList = {
+      groupA : {
+        isVisible : true,
+        data : null,
+        show : function(){
+          this.isVisible = true;
+          c.meetingsList.groupB.isVisible = false;
+        }
+      },
+      groupB : {
+        isVisible : false,
+        data : null,
+        show : function(){
+          this.isVisible = true;
+          c.meetingsList.groupA.isVisible = false;
+        }
+      }
+    };
+    c.logout = function(){
         logoutService.logout('/usignin');
     }
   }
