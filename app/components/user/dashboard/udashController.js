b7600df90847e759aaacccc9c9ca3c0091714d00
giorgiopea <annatar93@gmail.com>
2015-12-05 12:05:35 +0100

    start playing with free time slots

--- app/components/user/dashboard/udashController.js
+++ app/components/user/dashboard/udashController.js
@@ -28,9 +28,49 @@
         }
       }
     };
+    c.schedulesList = {
+      groupA : {
+        isVisible : true,
+        data : null,
+        show : function(){
+          this.isVisible = true;
+          c.schedulesList.groupB.isVisible = false;
+        }
+      },
+      groupB : {
+        isVisible : false,
+        data : null,
+        show : function(){
+          this.isVisible = true;
+          c.schedulesList.groupA.isVisible = false;
+        }
+      },
+    };
     c.logout = function(){
         logoutService.logout('/usignin');
     }
+    jQuery('#calendar').fullCalendar({
+      header: {
+        center: 'prev,next,today',
+        right: 'month,agendaWeek,agendaDay'
+      },
+
+      events: [
+        {
+            title  : 'event1',
+            start  : '2016-01-01',
+            editable : true
+        },
+        {
+            title  : 'event2',
+            start  : '2016-01-05',
+        },
+        {
+            title  : 'event3',
+            start  : '2016-01-09T12:30:00',
+            allDay : false // will make the time show
+        }
+    ]})
   };
 
   var app = angular.module('Plunner');
