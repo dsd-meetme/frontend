03aeb0d7ad934037a08192ffaf33a2786b81f20c
giorgiopea <annatar93@gmail.com>
2015-12-09 17:01:46 +0100

    introduced a general directive for logout

--- app/components/organization/profile/opController.js
+++ app/components/organization/profile/opController.js
@@ -1,20 +1,45 @@
 (function(){
-  var controller = function(orgResources,$scope){
+  var controller = function($scope,orgResources,mixedContentToArray){
     var c = this;
     c.data = {};
-    c.errors = {
-      unauthorized : false,
-      forbidden : false
-    };
+    c.dataCopy = {};
+    c.errors = [];
     c.getInfo = function(){
       orgResources.company().query().$promise
       .then(
         function(response){
           c.data = response;
+          c.dataCopy.name = response.name;
         });
       }
-    };
+      c.editProfile = {
+        name : c.dataCopy.name,
+        invalidFields : {
+          passwordLength : false,
+          passwordMatch : false
+        },
+        submit : function(){
+          var form = $scope.editProfileForm;
+          this.invalidFields.passwordLength = form.password.$error.minlength;
+          this.invalidFields.passwordMatch = (this.password !== this.password_confirmation);
+          if(!form.$invalid && !this.invalidFields.passwordMatch){
+            orgResources.organization.update().$promise
+            .then(
+              function(){
+                jQuery('#editProfile').modal('hide');
+                jQuery('#confirmPopup').modal('show');
+                $timeout(function(){
+                  jQuery('#confirmPopup').modal('hide');
+                },2000);
+                jQuery('#editProfile input').val('');
+                c.editProfile.name = c.dataCopy.name;
+              }
+            )
+          }
+        }
 
+      }
+    }
     var app = angular.module('Plunner');
     app.controller('opController',controller);
   }());
