1e5e1f8a95ef9cf90da2b6a59e4fff9123a2cbea
giorgiopea <annatar93@gmail.com>
2015-12-30 18:01:47 +0100

    completing responsive design

--- app/components/organization/profile/opController.js
+++ app/components/organization/profile/opController.js
@@ -14,7 +14,17 @@
                 .then(function(response){
                     c.data.name = response.name;
                     c.data.email = response.email;
+                    c.dataCopy.name = response.name;
                 });
+        };
+        c.confirmPopup = {
+            message : '',
+            show : function(){
+                jQuery('#authorizationPopup').modal('show');
+            },
+            hide : function(){
+                jQuery('#authorizationPopup').modal('hide');
+            }
         };
         c.editMode = {
             flag: false,
@@ -25,6 +35,7 @@
                 c.update.errors = [];
                 c.update.invalidFields.passwordMatch = false;
                 c.update.invalidFields.passwordLength = false;
+                c.update.invalidFields.nameReq = false;
                 c.dataCopy.password = '';
                 c.dataCopy.password_confirmation = '';
                 this.flag = false;
@@ -41,31 +52,37 @@
             },
             errors : [],
             submit : function(){
+                var toSend;
                 var form = $scope.opC_profile_form;
                 this.invalidFields.passwordLength = form.password.$error.minlength;
                 this.invalidFields.passwordMatch = c.dataCopy.password !== c.dataCopy.password_confirmation;
+                this.invalidFields.nameReq = form.name.$error.required;
                 if(!form.$invalid && !this.invalidFields.passwordMatch){
-                    if(c.dataCopy.password === ''){
+                    if(c.dataCopy.password === '' && (c.dataCopy.name !== c.data.name)){
                         c.editMode.exit();
                     }
                     else{
-                        jQuery('#authorizationPopup').modal('show');
-                        orgResources.employee().update(jQuery.param({
-                            name : c.data.name,
-                            email : c.data.email,
-                            password : c.dataCopy.password,
-                            password_confirmation : c.dataCopy.password
-                        })).$promise
-                            .then(function(response){
+                        c.confirmPopup.show();
+                        toSend = {
+                            name : c.dataCopy.name,
+                            email : c.data.email
+                        };
+                        if(c.dataCopy.password !== ''){
+                            toSend.password = c.dataCopy.password;
+                            toSend.password_confirmation = c.dataCopy.password;
+                        }
+                        orgResources.employee().update(jQuery.param(toSend)).$promise
+                            .then(function(){
                                 c.dataCopy.password = '';
                                 c.dataCopy.password_confirmation = '';
                                 //Update view
                                 c.getInfo();
                                 c.editMode.exit();
-                                jQuery('#authorizationPopup').modal('hide');
+                                c.confirmPopup.hide();
                             },function(response){
                                 if(response.status === 422){
                                     mixedContentToArray.process(response.data, c.update.errors, true);
+                                    c.confirmPopup.hide();
                                 }
                             })
                     }
--- app/components/user/profile/upController.js
+++ app/components/user/profile/upController.js
@@ -1,19 +1,30 @@
-(function() {
+(function () {
 
-    var controller = function ($scope,$timeout,orgResources,mixedContentToArray) {
+    var controller = function ($scope, $timeout, orgResources, mixedContentToArray) {
 
         var c = this;
 
         c.data = {
-            name : '',
-            email : ''
+            name: '',
+            email: ''
         };
 
-        c.getInfo = function(){
+        c.confirmPopup = {
+            message: '',
+            show: function () {
+                jQuery('#authorizationPopup').modal('show');
+            },
+            hide: function () {
+                jQuery('#authorizationPopup').modal('hide');
+            }
+        };
+
+        c.getInfo = function () {
             orgResources.employee().get().$promise
-                .then(function(response){
+                .then(function (response) {
                     c.data.name = response.name;
                     c.data.email = response.email;
+                    c.dataCopy.name = response.name;
                 });
         };
         c.editMode = {
@@ -25,53 +36,59 @@
                 c.update.errors = [];
                 c.update.invalidFields.passwordMatch = false;
                 c.update.invalidFields.passwordLength = false;
+                c.update.invalidFields.nameReq = false;
                 c.dataCopy.password = '';
                 c.dataCopy.password_confirmation = '';
                 this.flag = false;
             }
         };
         c.dataCopy = {
-            password : '',
-            password_confirmation : ''
+            name: '',
+            password: '',
+            password_confirmation: ''
         };
         c.update = {
-            invalidFields : {
-                passwordLength : false,
-                passwordMatch : false
+            invalidFields: {
+                passwordLength: false,
+                passwordMatch: false
             },
-            errors : [],
-            submit : function(){
+            errors: [],
+            submit: function () {
+                var toSend;
                 var form = $scope.upC_profile_form;
                 this.invalidFields.passwordLength = form.password.$error.minlength;
                 this.invalidFields.passwordMatch = c.dataCopy.password !== c.dataCopy.password_confirmation;
-                if(!form.$invalid && !this.invalidFields.passwordMatch){
-                    if(c.dataCopy.password === ''){
+                this.invalidFields.nameReq = form.name.$error.required;
+                if (!form.$invalid && !this.invalidFields.passwordMatch) {
+                    if((c.dataCopy.name !== c.data.name) && c.dataCopy.password === ''){
                         c.editMode.exit();
                     }
-                    else{
-
-                        orgResources.employee().update(jQuery.param({
-                            name : c.data.name,
-                            email : c.data.email,
-                            password : c.dataCopy.password,
-                            password_confirmation : c.dataCopy.password
-                        })).$promise
-                            .then(function(response){
-                                jQuery('#confirmPopup').modal('show');
-                                $timeout(function(){
-                                    jQuery('#confirmPopup').modal('hide')
-                                },2000);
+                    else {
+                        toSend = {
+                            name : c.dataCopy.name,
+                            email : c.data.name,
+                        };
+                        if(c.dataCopy.password !== ''){
+                            toSend.password = c.dataCopy.password;
+                            toSend.password_confirmation = c.dataCopy.password;
+                        }
+                        c.confirmPopup.show();
+                        orgResources.employee().update(jQuery.param(toSend)).$promise
+                            .then(function (response) {
                                 c.dataCopy.password = '';
                                 c.dataCopy.password_confirmation = '';
                                 //Update view
                                 c.getInfo();
                                 c.editMode.exit();
-                            },function(response){
-                                if(response.status === 422){
+                                c.confirmPopup.hide();
+                            }, function (response) {
+                                if (response.status === 422) {
                                     mixedContentToArray.process(response.data, c.update.errors, true);
+                                    c.confirmPopup.hide();
                                 }
                             })
                     }
+
                 }
             }
 
